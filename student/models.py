a0d44fc5a393f62b0599a3746e9bde46fb55cd80
ts <rna.globaliasoft@gmail.com>
2022-06-10 12:53:28 +0530

    ne

--- student/models.py
+++ student/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Student(models.Model):
     name = models. CharField(max_length=255)
     roll = models.IntegerField()
-    city = models.CharField(max_length=255)
+    city = models.CharField(max_length=60)
